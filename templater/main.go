d17b71f849cc08c256b2f88800fa306db037c793
Azat Kalmykov <azotx12345@gmail.com>
2020-09-25 19:13:48 +0300

    some changes

---
+++ templater/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+	"path"
+	"text/template"
+)
+
+type Version struct {
+	Id           string `yaml:"id"`
+	ImageVersion string `yaml:"imageVersion"`
+}
+
+var executorDeploymentTemplateFileName = "../executor-deployment-template.yaml"
+var executorDeploymentsFileName = "../executor-deployments.yaml"
+
+var autoscalerTemplateFileName =  "../autoscaler-template.yaml"
+var autoscalersFileName = "../autoscalers.yaml"
+
+func main() {
+	configFile, err := os.OpenFile("../config/versions.yaml", os.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decoder := yaml.NewDecoder(configFile)
+
+	data := struct {
+		Versions []Version `yaml:"versions"`
+	}{}
+
+	if err := decoder.Decode(&data); err != nil {
+		log.Fatal(err)
+	}
+
+	executorDeploymentTemplate, err := template.New(path.Base(executorDeploymentTemplateFileName)).
+		ParseFiles(executorDeploymentTemplateFileName)
+	executorDeploymentsFile, err := os.OpenFile(executorDeploymentsFileName, os.O_WRONLY | os.O_CREATE, 0644)
+	if err := executorDeploymentTemplate.Execute(executorDeploymentsFile, data); err != nil {
+		log.Fatal(err)
+	}
+
+	
+
+
+}
